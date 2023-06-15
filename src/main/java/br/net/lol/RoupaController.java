2569261e5d96afa4f92ec5c9c4f418c020d5b3a6
PedroHorning <pedro.horning2546@gmail.com>
2023-06-15 20:36:30 -0300

    criado crud de roupa

--- src/main/java/br/net/lol/RoupaController.java
+++ src/main/java/br/net/lol/RoupaController.java
@@ -1,11 +1,17 @@
 package br.net.lol;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
@@ -20,6 +26,36 @@ public class RoupaController {
     @GetMapping("/roupas")
     public List<Roupa> getRoupas() {
         return roupaRepository.findAll();
+    }
+    
+    @PostMapping("/roupas/add")
+    public Roupa addRoupa(@RequestBody Roupa roupa) {
+        return roupaRepository.save(roupa);
+    }
+
+    @PutMapping("/roupas/update/{id}")
+    public Roupa updateRoupa(@PathVariable Integer id, @RequestBody Roupa roupa) {
+        Optional<Roupa> existingRoupa = roupaRepository.findById(id);
+        if (existingRoupa.isPresent()) {
+            Roupa updatedRoupa = existingRoupa.get();
+            updatedRoupa.setName(roupa.getName());
+            updatedRoupa.setDeadline(roupa.getDeadline());
+            updatedRoupa.setPrice(roupa.getPrice());
+            return roupaRepository.save(updatedRoupa);
+        } else {
+            throw new RuntimeException("Roupa not found with id: " + id);
+        }
+    }
+
+    @DeleteMapping("/roupas/delete/{id}")
+    public String deleteRoupa(@PathVariable Integer id) {
+        Optional<Roupa> existingRoupa = roupaRepository.findById(id);
+        if (existingRoupa.isPresent()) {
+            roupaRepository.delete(existingRoupa.get());
+            return "Roupa deleted successfully.";
+        } else {
+            throw new RuntimeException("Roupa not found with id: " + id);
+        }
     }
 
     @GetMapping("/error")
